3e9385a486cf8c419c49eeaf2e6753889d90d584
Mauro Lopes <mauro.slopes06@gmail.com>
2024-09-27 12:11:44 -0300

    first commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System.ComponentModel;
+using System.Windows;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+
+namespace WPF_INotifyPropertyChanged
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window, INotifyPropertyChanged
+    {
+        public MainWindow()
+        {
+            DataContext = this;
+            InitializeComponent();
+        }
+
+        private string boundText;
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public string BoundText
+        {
+            get { return boundText; }
+            set { 
+                boundText = value;
+                OnPropertyChange();
+            }
+        }
+
+
+        void OnPropertyChange([CallerMemberName]string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private void btnSet_Click(object sender, RoutedEventArgs e)
+        {
+            BoundText = "Changed from code";
+        }
+    }
+}
